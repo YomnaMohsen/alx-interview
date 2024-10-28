d4de04da519040da51f2fe24cc915b0f6033b4d8
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-10-28 17:10:20 +0300

    interview-q

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Making change module"""
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """Calculates fewest number of coins
+    needed to meet a given amount total"""
+    if total <= 0:
+        return 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for a in range(1, total + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a-c])
+    return dp[total] if dp[total] != total + 1 else -1
