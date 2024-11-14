d7b9a934eadf64352c6b0f1b5a3134dc2fb9171a
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-14 23:23:15 +0200

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,14 +2,14 @@
 // display charcters in movie with certain id
 const request = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/${id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
 request(url, (error, response, body) => {
   if (!error & response.statusCode === 200) {
     const data = JSON.parse(body);
     const charactersurl = data.characters;
 
-    charactersurl.array.forEach(url => {
+    charactersurl.forEach(url => {
       request(url, (error, response, body) => {
         if (!error & response.statusCode === 200) {
           const charname = JSON.parse(body);
