c84e5b7e67b8da0bfac76cf55086862899fe643b
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-14 23:20:16 +0200

    starwar

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// display charcters in movie with certain id
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/${id}`;
+
+request(url, (error, response, body) => {
+  if (!error & response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const charactersurl = data.characters;
+
+    charactersurl.array.forEach(url => {
+      request(url, (error, response, body) => {
+        if (!error & response.statusCode === 200) {
+          const charname = JSON.parse(body);
+          console.log(charname.name);
+        } else {
+          console.log('error', error);
+        }
+      });
+    });
+  } else {
+    console.log('error', error);
+  }
+});
