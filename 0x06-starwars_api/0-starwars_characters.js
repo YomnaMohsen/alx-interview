8ec503db69ba21b7863aa1cd55b6fa236bfee36a
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-15 00:35:59 +0200

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,22 +4,38 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
-request(url, (error, response, body) => {
-  if (!error & response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const charactersurl = data.characters;
+const getchars = new Promise((resolve, reject) => {
+  request.get(url, (err, response, body) => {
+    if (!err) {
+      try {
+        resolve(JSON.parse(body).characters);
+      } catch (error) {
+        reject(error);
+      }
+    }
+    reject(err);
+  });
+});
 
-    charactersurl.forEach(url => {
-      request(url, (error, response, body) => {
-        if (!error & response.statusCode === 200) {
-          const charname = JSON.parse(body);
-          console.log(charname.name);
-        } else {
-          console.log('error', error);
-        }
-      });
-    });
-  } else {
-    console.log('error', error);
+getchars.then((data) => {
+  const chararr = [];
+  for (const ch of data) {
+    chararr.push(
+      new Promise((resolve, reject) => {
+        request.get(ch, (err, response, body) => {
+          if (err) reject(err);
+          try {
+            resolve(JSON.parse(body).name);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+    );
   }
+  Promise.all(chararr).then((names) => {
+    for (const n of names) {
+      console.log(n);
+    }
+  });
 });
