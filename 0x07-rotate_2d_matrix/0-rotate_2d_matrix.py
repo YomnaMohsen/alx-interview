13fa068b68c945e97a53cb012430ed8ca6b2310f
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-21 17:36:13 +0200

    style

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,9 +11,3 @@ def rotate_2d_matrix(matrix):
 
     for row in matrix:
         row.reverse()
-matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-
-rotate_2d_matrix(matrix)
-print(matrix)    
