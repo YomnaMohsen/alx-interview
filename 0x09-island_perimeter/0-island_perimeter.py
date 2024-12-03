c56a03762248d3e5da5759e012a5a79c96ba5d08
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-12-03 12:37:40 +0200

    fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,22 +7,26 @@ from typing import List
 def island_perimeter(grid: List[List[int]]) -> int:
     """return island perimeter"""
     visit = set()
-
-    def dfs(i, j):
-        """helper function using depth 1st search"""
-        if i >= len(grid) or j >= len(grid[0]) or \
-           i < 0 or j < 0 or grid[i][j] == 0:
-            return 1
-        if (i, j) in visit:
-            return 0
-        visit.add((i, j))
-        perim = dfs(i, j + 1)
-        perim += dfs(i, j - 1)
-        perim += dfs(i + 1, j)
-        perim += dfs(i - 1, j)
-        return perim
-
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
-                return dfs(i, j)
+                return dfs(i, j, grid, visit)
+
+
+
+
+def dfs(i, j, grid, visit):
+    """helper function using depth 1st search"""
+    if i >= len(grid) or j >= len(grid[0]) or \
+        i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+    if (i, j) in visit:
+        return 0
+    visit.add((i, j))
+    perim = dfs(i, j + 1)
+    perim += dfs(i, j - 1)
+    perim += dfs(i + 1, j)
+    perim += dfs(i - 1, j)
+    return perim
+
+    
